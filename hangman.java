25e3a84122a245b055fbe2830ae5bec6cd68f34e
Oceane01 <ocea.ducl@gmail.com>
2025-11-12 15:45:26 +0100

    finish hangman

--- hangman.java
+++ hangman.java
@@ -44,14 +44,13 @@ class Hangman {
     };
 
     public static void play(String word) {
+        boolean l = false;
         int round = 7;
         int letterTrouve = 0;
-        while (round != 0 || letterTrouve != word.length()) {
-            System.out.println(word);
+        while (round > 0 || letterTrouve != word.length()) {
             System.out.println("choissez une lettre: ");
             String let = scan.nextLine();
             char letter = let.charAt(0);
-            ArrayList<String> listLetter = new ArrayList<String>();
             for (int i = 0; i < word.length(); i++) {
                 char lettre = word.charAt(i);
                 // listLetter.add(lettre);
@@ -61,15 +60,20 @@ class Hangman {
                     System.out.println(
                             "La letter '" + letter + "' est bien présente dans le mot à la" + (i + 1) + " ème place");
                     letterTrouve++;
-                } else {
-                    System.out.println("la lettre " + letter + " n'est pas dans le mot. Recommencez");
-                    round--;
+                    l = true;
                 }
+            }
+            if (l == true) {
+                l= false;
+            }else{
+                System.out.println("la lettre " + letter + " n'est pas dans le mot. Recommencez");
+                round--;
             }
             if (letterTrouve == word.length()) {
                 System.out.println("Bravo vous avez trouvez le mot: " + word);
                 break;
-            } else if (round == 0) {
+            }
+            if (round == 0) {
                 System.out.println("Dommage le mot etait: " + word);
                 break;
             }
